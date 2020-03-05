ecd9a422ffad3ef092413a07ac0266e31457d202
Mohammad Shadab <shadab2266@gmail.com>
2020-03-05 23:38:29 +0530

    Initial commit

---
+++ Delegate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace c_sharp_core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Delegate demo gonna start!");
+            SampleMethod(24,
+                      delegate
+                      {
+                         Console.WriteLine("This is delegate action!");
+                      });
+            Console.WriteLine("Delegate has been passed to method");
+        }
+         public static void SampleMethod(int data, Action action) {
+          Console.WriteLine("Hey i am invoked to test delegate");
+          if (action != null)
+                    action();
+        Console.WriteLine("END");
+        }
+      }
+    }
+
